cbe2485b22a63ed8260f745fe2becb7f3d2f782a
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-04-26 19:44:45 +0200

    Parte html e css

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const root = new Vue({
+  el: "#root",
+  data: {},
+  methods: {},
+});
+
+const user = {
+  name: "Nome Utente",
+  avatar: "_io",
+};
+const contacts = [
+  {
+    name: "Michele",
+    avatar: "_1",
+  },
+  {
+    name: "Fabio",
+    avatar: "_2",
+  },
+  {
+    name: "Samuele",
+    avatar: "_3",
+  },
+  {
+    name: "Luisa",
+    avatar: "_4",
+  },
+];
